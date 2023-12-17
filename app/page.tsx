9b15583aa19978ed38df16493f20aff7e4e2be6b
9riousen <9riousen@gmail.com>
2023-12-17 22:20:16 +0900

    토스트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+'use client';
+
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { useToast } from '@/components/ui/use-toast';
 import Image from 'next/image'
+import { useState } from 'react'
+
+export const URL_FAUCET = 'https://faucet-ebonynw.line-apps.com/credit'
+
+const isValidAddress = (addr: string) => /^tlink1/.test(addr)
 
 export default function Home() {
+  const [addr, setAddr] = useState('');
+  const { toast } = useToast();
+
+  async function handleFaucetRequest() {
+    console.log('handleFaucetRequest', isValidAddress(addr));
+    const resp = await fetch(URL_FAUCET, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          denom: "tcony",
+          address: addr
+        })
+      });
+      const respText = await resp.text();
+      toast({
+        title: resp.ok ? "성공" : "실패",
+        description: resp.ok ? `+10TFNSA ${addr}` : respText,
+        variant: resp.ok ? null : 'destructive'
+      });
+  }
+
   return (
     <main className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6'>
       <Card className='w-full max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden dark:bg-gray-800'>
@@ -20,21 +51,21 @@ export default function Home() {
             테스트코인을 받을 주소를 입력하세요(1일 1회)
           </p>
           <div className='p-6'>
-            <form>
-              <div className='flex flex-col gap-4'>
-                <label
-                  htmlFor='walletAddress'
-                  className='text-sm font-medium text-gray-900 dark:text-white'>주소</label>
-                <Input
-                  className='w-full bg-gray-100 dark:bg-gray-950'
-                  id='walletAddress'
-                  placeholder='tlink1????'
-                />
-                <Button>
-                  10TFNSA 주세요
-                </Button>
-              </div>
-            </form>
+            <div className='flex flex-col gap-4'>
+              <label
+                htmlFor='walletAddress'
+                className='text-sm font-medium text-gray-900 dark:text-white'>주소</label>
+              <Input
+                className='w-full bg-gray-100 dark:bg-gray-950'
+                id='walletAddress'
+                placeholder='tlink1????'
+                value={addr}
+                onChange={e => setAddr(e.target.value)}
+              />
+              <Button onClick={handleFaucetRequest}>
+                10TFNSA 주세요
+              </Button>
+            </div>
           </div>
         </CardContent>
       </Card>
